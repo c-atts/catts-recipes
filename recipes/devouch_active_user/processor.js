086891c88cb813c9c6e1805e327ad4bd2731dba9
Kristofer <kristofer@fmckl.se>
2024-09-06 15:07:14 +0200

    devouch-active-user: Fix error

--- recipes/devouch_active_user/processor.js
+++ recipes/devouch_active_user/processor.js
@@ -1,6 +1,6 @@
 let vouchCount = queryResult[0].data.aggregateAttestation?._count?.id;
 
-if (!vouchCount || vouchCount < 1) {
+if (!vouchCount || vouchCount < 10) {
   throw new Error("You need at least 10 vouches to create this attestation.");
 }
 
